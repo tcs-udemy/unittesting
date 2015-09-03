68f956889e05ffd7c19bad25c3450d270c489928
Trevor Sawler <trevor.sawler@gmail.com>
2015-09-03 12:09:14 -0300

    Simple public pages tests

---
+++ src/Tests/AdminControllerTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace Acme\Tests;
+
+class AdminControllerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetShowPage()
+    {
+        $test = false;
+        $this->assertFalse($test);
+    }
+}
