a741d605261f0da3b853b285b53fe251f05ca69b
Trevor Sawler <trevor.sawler@gmail.com>
2015-09-04 16:41:11 -0300

    add stub

---
+++ src/Tests/UserTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace Acme\Tests;
+
+use Acme\Models\User;
+
+class UserTest extends AcmeBaseTest
+{
+
+    public function testTestimonials()
+    {
+//        $user = User::find(1);
+//        $expected = "Trevor";
+//        $returned = get_class($user->testimonials);
+//        $this->assertEquals($expected, $returned);
+    }
+
+}
